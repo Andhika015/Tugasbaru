897514c2250708eacaceb828d2564cfc78a3c272
Andhika015 <135310453+Andhika015@users.noreply.github.com>
2023-06-02 16:33:04 +0700

    Add files via upload

---
+++ bilangan.js
@@ -0,0 +1,9 @@
+let input = prompt("Masukkan Bilangan");
+    if ( isNaN(input)){
+	 alert(input = "Bukan Bilangan!")
+       }else{if(input % 2 === 0){
+	      alert(input + "adalah bilangan genap")
+       }else{
+	      alert(input + "adalah bilangan ganjl")
+ }
+}
